ba34c64fede30138af60cc051cbc5506cbad112a
Chaitanya Raj <rajchaitanya75@gmail.com>
2025-07-25 00:50:40 +0530

    logo and bugs

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -12,7 +12,6 @@ function Card(props) {
         <div onClick={() => { handleClick() }} className="card-mov rounded-t-2xl cursor-pointer bg-amber-50 m-4 mr-0 w-35 h-70 overflow-hidden">
             <div className="w-full h-50 overflow-hidden">
                 <img className="bg-cover w-full rounded-t-2xl" src={`https://image.tmdb.org/t/p/w342${props.movie.poster_path}`} alt="" />
-                {/* <img className="bg-cover w-full rounded-t-2xl" src={props.movie.poster} alt="" /> */}
             </div>
             <p className="font-baloo2 text-black p-1 text-lg/5 max-h-12 overflow-x-auto hidescrollbar">{props.type == "movie" ? props.movie.title : props.movie.name}</p>
             <p className="font-inter text-gray-500 pl-1 text-sm">{date}</p>
